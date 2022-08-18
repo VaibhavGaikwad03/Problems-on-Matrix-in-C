7072f5ea3e00d50c1218d67974445f28b434fa42
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-08-18 10:52:57 +0530

    Add files via upload

---
+++ Swap_Rows.c
@@ -0,0 +1,72 @@
+/*
+Write a program which accepts matrix from user and swap the contents of consecutive rows.
+
+Input : 
+
+3   2   5   9
+4   3   2   2
+8   4   1   9
+3   9   7   5
+
+Output : 
+
+4   3   2   2
+3   2   5   9
+3   9   7   5
+8   4   1   9
+
+
+*/
+
+#include<stdio.h>
+
+void SwapRows(int iRow, int iCol, int Matrix[][iCol])
+{
+    int i = 0, j = 0, iTemp = 0;
+
+    for(i = 0; i < iRow-1; i+=2)
+    {   
+        for(j = 0; j < iCol; j++)
+        {
+            iTemp = Matrix[i][j];
+            Matrix[i][j] = Matrix[i+1][j];
+            Matrix[i+1][j] = iTemp; 
+        }
+    }
+
+    for(i = 0; i < iRow; i++)
+    {   
+        for(j = 0; j < iCol; j++)
+        {
+            printf("%d\t", Matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iRow = 0, iCol = 0;
+    int i = 0, j = 0;
+
+    printf("Enter the number of rows : \n");
+    scanf("%d", &iRow);
+
+    printf("Enter the number of columns : \n");
+    scanf("%d", &iCol);
+
+    int Arr[iRow][iCol];
+
+    printf("Enter the elements in Matrix : \n");
+    for(i = 0; i < iRow; i++)
+    {
+        for(j = 0; j < iCol; j++)
+        {
+            scanf("%d", &Arr[i][j]);
+        }
+    }
+    
+    SwapRows(iRow, iCol, Arr);
+
+    return 0;
+}
