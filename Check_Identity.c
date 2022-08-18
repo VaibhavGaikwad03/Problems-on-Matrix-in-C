193cf8edc935888824db1b199d790c3d03382ab8
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-08-18 12:39:56 +0530

    Add files via upload

--- Check_Identity.c
+++ Check_Identity.c
@@ -1,6 +1,9 @@
 /*
 Write a program which accepts matrix from user and check whether the matix is identity matrix or not.
 
+Identity matrix is a square matrix with 1's along the diagonal from upper left to lower right and 0's in all other positions.
+If it satisfies the structure as explained before then the matrix is called as identity matrix.
+
 Input : 
 
 1   0   0   0
